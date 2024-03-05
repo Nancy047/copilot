f2935b2fdc53709056f9d7ee05a29ef0bbb65d94
Nancy047 <117806579+Nancy047@users.noreply.github.com>
2024-03-05 21:36:33 +0530

    Update MainSidebar.jsx

--- src/components/MainSidebar.jsx
+++ src/components/MainSidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "/styles/Sidebar.scss";
+import Sidebar from "./Sidemenu";"/styles/Sidebar.scss";
 
 import { RiHome2Fill } from "react-icons/ri";
 import { BiSolidMessageSquareDetail } from "react-icons/bi";
